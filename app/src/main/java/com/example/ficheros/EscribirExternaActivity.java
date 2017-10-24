161b42e1991824f5ddef283c3fd1bac881a21cc4
Carlos Cruz Domínguez <carloscruzdominguez97@gmail.com>
2017-10-24 12:26:53 +0200

    Subo

--- app/src/main/java/com/example/ficheros/EscribirExternaActivity.java
+++ app/src/main/java/com/example/ficheros/EscribirExternaActivity.java
@@ -2,12 +2,56 @@ package com.example.ficheros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class EscribirExternaActivity extends AppCompatActivity {
+public class EscribirExternaActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText edtOperando1, edtOperando2;
+    TextView txvResultado, txvInfoFichero;
+    Button btnCalcular;
+    public final static String NOMBREFICHERO = "resultado.txt";
+    Memoria miMemoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escribir_externa);
+
+        edtOperando1 = (EditText)findViewById(R.id.edtOperando1);
+        edtOperando2 = (EditText)findViewById(R.id.edtOperando2);
+        txvResultado = (TextView)findViewById(R.id.txvResultado);
+        txvInfoFichero = (TextView)findViewById(R.id.txvInfoFichero);
+        btnCalcular = (Button)findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(this);
+
+        miMemoria = new Memoria(getApplicationContext());
+    }
+
+    @Override
+    public void onClick(View view) {
+        int r;
+        String op1 = edtOperando1.getText().toString();
+        String op2 = edtOperando2.getText().toString();
+        String texto;
+
+        if (view == btnCalcular) {
+            try {
+                r = Integer.parseInt(op1) + Integer.parseInt(op2);
+                texto = Integer.toString(r);
+                txvResultado.setText(texto);
+                if (miMemoria.escribirExterna(NOMBREFICHERO, texto, false, "UTF-8")) {
+                    txvInfoFichero.setText(miMemoria.mostrarPropiedadesExterna(NOMBREFICHERO));
+                } else {
+                    txvInfoFichero.setText("Error al escribir en el fichero " + NOMBREFICHERO);
+                }
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Error en el formato de los números", Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/ficheros/LeerFicherosActivity.java
+++ app/src/main/java/com/example/ficheros/LeerFicherosActivity.java
@@ -2,12 +2,130 @@ package com.example.ficheros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LeerFicherosActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class LeerFicherosActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText edtLeerRaw;
+    private EditText edtLeerAsset;
+    private EditText edtLeerInterna;
+    private EditText edtLeerExterna;
+    private TextView txvCalculo;
+    private Button btnCalcular;
+    //El openRawResource no tiene que llevar la extensión.
+    public static final String NUMERO = "numero";
+    public static final String VALOR = "valor.txt";
+    public static final String DATO = "datos.txt";
+    public static final String DATO_SD = "dato_sd.txt";
+    public static final String OPERACIONES = "operaciones.txt";
+    public static final String CODIFICACION = "UTF-8";
+    Memoria memoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leer_ficheros);
+
+        iniciar();
+    }
+
+    private void iniciar() {
+        Resultado resultado;
+
+        edtLeerRaw = (EditText) findViewById(R.id.edtLeerRaw);
+        edtLeerAsset = (EditText) findViewById(R.id.edtLeerAsset);
+        edtLeerInterna = (EditText) findViewById(R.id.edtLeerInterna);
+        edtLeerExterna = (EditText) findViewById(R.id.edtLeerExterna);
+        txvCalculo = (TextView) findViewById(R.id.txvCalculo);
+        btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(this);
+        memoria = new Memoria(getApplicationContext());
+
+        resultado = memoria.leerRaw(NUMERO);
+        if (resultado.getCodigo()) {
+            edtLeerRaw.setText(resultado.getContenido());
+        } else {
+            edtLeerRaw.setText("0");
+            Toast.makeText(this, "Error al leer " + NUMERO + " " + resultado.getMensaje(), Toast.LENGTH_SHORT).show();
+        }
+
+        resultado = memoria.leerAsset(VALOR);
+        if (resultado.getCodigo()) {
+            edtLeerAsset.setText(resultado.getContenido());
+        } else {
+            edtLeerAsset.setText("0");
+            Toast.makeText(this, "Error al leer " + VALOR + " " + resultado.getMensaje(), Toast.LENGTH_SHORT).show();
+        }
+
+        if (memoria.escribirInterna(DATO, "7", false, CODIFICACION)) {
+            resultado = memoria.leerInterna(DATO, CODIFICACION);
+            if (resultado.getCodigo()) {
+                edtLeerInterna.setText(resultado.getContenido());
+            } else {
+                edtLeerInterna.setText("0");
+                Toast.makeText(this, "Error al leer " + DATO + " " + resultado.getMensaje(), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Error al escribir " + DATO, Toast.LENGTH_SHORT).show();
+        }
+
+        if (memoria.disponibleEscritura()) {
+            if (memoria.escribirExterna(DATO_SD, "5", false, CODIFICACION)) {
+                resultado = memoria.leerExterna(DATO_SD, CODIFICACION);
+                if (resultado.getCodigo()) {
+                    edtLeerExterna.setText(resultado.getContenido());
+                } else {
+                    edtLeerExterna.setText("0");
+                    Toast.makeText(this, "Error al leer " + DATO_SD + " " + resultado.getMensaje(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Error al escribir " + DATO_SD, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "La memoria externa no está disponible", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnCalcular) {
+            String operando1 = edtLeerRaw.getText().toString();
+            String operando2 = edtLeerAsset.getText().toString();
+            String operando3 = edtLeerInterna.getText().toString();
+            String operando4 = edtLeerExterna.getText().toString();
+
+            String operacion;
+            try {
+                int resultado = Integer.parseInt(operando1) +
+                        Integer.parseInt(operando2) +
+                        Integer.parseInt(operando3) +
+                        Integer.parseInt(operando4);
+
+                operacion = Integer.parseInt(edtLeerRaw.getText().toString()) + "+" +
+                        Integer.parseInt(edtLeerAsset.getText().toString()) + "+" +
+                        Integer.parseInt(edtLeerInterna.getText().toString()) + "+" +
+                        Integer.parseInt(edtLeerExterna.getText().toString()) + "=" + resultado;
+
+                txvCalculo.setText(operacion);
+
+                if (memoria.disponibleEscritura()) {
+                    if (memoria.escribirExterna(OPERACIONES, operacion + "\n", true, CODIFICACION)) {
+                        Toast.makeText(this, "Operación escrita correctamente", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Error al escribir en la memoria externa", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Error al convertir las cifras", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
